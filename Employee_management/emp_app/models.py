c69f4f8637583a8e230645deac70d16143818da2
pranavd17 <pranavdurugkar@gmail.com>
2023-05-31 18:07:38 +0530

    Doc string added in model

--- Employee_management/emp_app/models.py
+++ Employee_management/emp_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Department(models.Model):
     name = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=100)
@@ -8,11 +10,13 @@ class Department(models.Model):
     def __str__(self):
         return self.name
 
+
 class Role(models.Model):
     name = models.CharField(max_length=100, null=False)
 
     def __str__(self):
         return self.name
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100, null=False)
@@ -25,7 +29,5 @@ class Employee(models.Model):
     hire_date = models.DateField()
 
     def __str__(self):
-        return  "%s %s %s" %(self.first_name, self.last_name, self.phone)
-
-
-    
+        '''for displaying fields on admin panel'''
+        return "%s %s %s" % (self.first_name, self.last_name, self.phone)
